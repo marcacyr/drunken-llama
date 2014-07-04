036ba503e0acefaf16902807a3de41109af96598
Marc Cyr <marcacyr@gmail.com>
2014-07-03 20:10:28 -0700

    update view controller for refreshing UI

--- Tip Calculator/TipCalculatorModel.swift
+++ Tip Calculator/TipCalculatorModel.swift
@@ -35,7 +35,6 @@ class TipCalculatorModel {
         var retval = Dictionary<Int, Double>()
         for possibleTip in possibleTipsInferred {
             let intPct = Int(possibleTip*100)
-            // 3
             retval[intPct] = calcTipWithTipPct(possibleTip)
         }
         return retval
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,21 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var totalTextFixed : UITextField
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        totalTextField.text = String(tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
+    
+    @IBOutlet var totalTextField : UITextField
     @IBOutlet var taxPctSlider : UISlider
     @IBOutlet var taxPctLabel : UILabel
     @IBOutlet var resultsTextView : UITextView
     
     @IBAction func calculateTapped(sender : AnyObject) {
+        tipCalc.total = Double(totalTextField.text.bridgeToObjectiveC().doubleValue)
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        resultsTextView.text = results
     }
     @IBAction func taxPercentageChanged(sender : AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
     }
     @IBAction func viewTapped(sender : AnyObject) {
+        totalTextField.resignFirstResponder()
     }
-                            
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
